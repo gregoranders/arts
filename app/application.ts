4778fd82fb9ea713f7e9a97e3d512296045812e5
Gregor Anders <gregor.anders@gmail.com>
2015-03-29 22:26:33 +0200

    * added bottom sheet support

--- app/application.ts
+++ app/application.ts
@@ -2,6 +2,7 @@
 
 import Arts = require("./components/arts/Arts");
 
+
 interface IScope extends Arts.IScope<IController> {
 
 }
@@ -10,29 +11,54 @@ interface IController extends Arts.IController<IScope> {
   selectedTabIndex: number;
 
   switchToTab(tab: number): void;
+  switchToTabFromBottomSheet(tab: number): void;
+
   toggleSideBar(id: string): void;
 
   refresh(): void;
 
   success(): void;
   error(): void;
+
+  bottomSheet(): void;
 }
 
 class IndexController extends Arts.BaseController<IScope> implements IController {
 
-  static $inject:string[] = ['$scope', '$mdSidenav', '$mdToast'];
+  static $inject:string[] = ['$scope', '$mdSidenav', '$mdToast', '$mdBottomSheet'];
 
   selectedTabIndex: number = 0;
 
+  bottomSheetPromise: ng.IPromise<void>;
+
   constructor(public $scope:IScope, private $mdSidenav:ng.material.MDSidenavService,
-    private $mdToast: ng.material.MDToastService) {
+    private $mdToast: ng.material.MDToastService,
+    private $mdBottomSheet: ng.material.MDBottomSheetService) {
     super($scope);
+  }
+
+  bottomSheet(): void {
+    this.bottomSheetPromise = this.$mdBottomSheet.show({
+      templateUrl: 'view/main-bottom-sheet.html',
+      scope: this.$scope,
+      preserveScope: true,
+      parent: <any>angular.element(document.getElementById('content'))
+    });
   }
 
   switchToTab(tab: number): void {
     this.selectedTabIndex = tab;
     this.$mdSidenav('left').close();
   }
+
+  switchToTabFromBottomSheet(tab: number): void {
+    if (this.bottomSheetPromise) {
+      this.$mdBottomSheet.hide(this.bottomSheetPromise);
+      this.bottomSheetPromise = null;
+    }
+    this.selectedTabIndex = tab;
+  }
+
 
   toggleSideBar(id: string): void {
     this.$mdSidenav(id).toggle();
