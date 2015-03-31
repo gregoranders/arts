99e449829e7d6ec96636b12b87a1ec94761632c6
Gregor Anders <gregor.anders@gmail.com>
2015-03-31 17:58:35 +0200

    Closes #2 Make selected tab persistent  - final implementation

--- app/components/docs/controller/IndexController.ts
+++ app/components/docs/controller/IndexController.ts
@@ -15,12 +15,25 @@ interface IController extends Arts.IController<IScope> {
 
   toggleSideBar(id:string): void;
 
+  tabSelected(group:Model.IDocsGroup): void;
   switchToTab(group:Model.IDocsGroup): void;
 }
 
 class IndexController extends Arts.BaseController<IScope> implements IController {
 
-  static $inject:string[] = ['$scope', '$mdSidenav', '$mdToast', '$mdBottomSheet', Component.SERVICE];
+  static NAME: string = Component.NAME + '.controller.index';
+
+  static TABS_NAME: string = 'docs.controller.index.tab';
+
+  static $inject:string[] = [
+    '$scope',
+    '$mdSidenav',
+    '$mdToast',
+    '$mdBottomSheet',
+    'localStorageService',
+    Component.SERVICE];
+
+  static DEFAULT_TAB: number = 0;
 
   selectedTabIndex:number = 0;
 
@@ -37,10 +50,17 @@ class IndexController extends Arts.BaseController<IScope> implements IController
   constructor(public $scope:IScope, private $mdSidenav:ng.material.MDSidenavService,
               private $mdToast:ng.material.MDToastService,
               private $mdBottomSheet:ng.material.MDBottomSheetService,
+              private localStorageService:angular.local.storage.ILocalStorageService<number>,
               private IDocsService:IDocsService) {
     super($scope);
 
     var component:Arts.IApplication = <Arts.IApplication>Arts.Arts.getModule(Component.NAME);
+
+    this.selectedTabIndex = localStorageService.get(IndexController.TABS_NAME);
+
+    if (!this.selectedTabIndex) {
+      this.selectedTabIndex = IndexController.DEFAULT_TAB;
+    }
 
     this.baseURL = component.getBaseURL();
 
@@ -55,6 +75,10 @@ class IndexController extends Arts.BaseController<IScope> implements IController
 
   toggleSideBar(id:string):void {
     this.$mdSidenav(id).toggle();
+  }
+
+  tabSelected(group:Model.IDocsGroup): void {
+    this.localStorageService.set(IndexController.TABS_NAME, this.docs.groups.indexOf(group));
   }
 
   switchToTab(group:Model.IDocsGroup):void {
